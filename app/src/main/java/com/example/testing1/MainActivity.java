786f94cc1465e6347757751b1743261ae0368dad
John Ken Ompad <johnken2x13@gmail.com>
2023-02-11 13:16:33 +0800

    Hello I am John Ken Ompad

--- app/src/main/java/com/example/testing1/MainActivity.java
+++ app/src/main/java/com/example/testing1/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.testing1;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG,"This is a verbose log.");
+        Log.d(TAG,"This is a debug log.");
+        Log.i(TAG,"This is an info log.");
+        Log.w(TAG,"This is a warn log.");
+        Log.e(TAG,"This is an error log.");
+
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"Button Clicker");
+            }
+        });
     }
 }
